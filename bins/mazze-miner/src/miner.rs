9a6e1420e1b88349535d35d0cab0599c6909b8f6
0xnotadev <nota.dev@yahoo.com>
2024-11-04 10:21:45 +0100

    refactored mining process

--- bins/mazze-miner/src/miner.rs
+++ bins/mazze-miner/src/miner.rs
@@ -1,18 +1,19 @@
 use log::{info, trace};
 use mazze_types::{H256, U256};
 use mazzecore::pow::{
-    boundary_to_difficulty, PowComputer, ProofOfWorkProblem,
-    ProofOfWorkSolution,
+    boundary_to_difficulty, ProofOfWorkProblem, ProofOfWorkSolution,
 };
+use randomx_rs::{RandomXCache, RandomXFlag, RandomXVM};
 use serde_json::Value;
 use std::str::FromStr;
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{mpsc, Arc, RwLock};
 use std::thread;
-use std::time::Duration;
+use std::time::Instant;
+
+const CHECK_INTERVAL: u64 = 100; // Check for new problem every 100 nonces
 
 struct MiningState {
     current_problem: Option<ProofOfWorkProblem>,
-    is_solved: bool,
     solution_sender: Option<mpsc::Sender<ProofOfWorkSolution>>,
 }
 
@@ -20,23 +21,20 @@ struct MiningState {
 pub struct Miner {
     pub worker_id: usize,
     pub worker_name: String,
-    pow_computer: Arc<PowComputer>,
     num_threads: usize,
-    state: Arc<Mutex<MiningState>>,
+    state: Arc<RwLock<MiningState>>,
 }
 
 impl Miner {
     pub fn new(num_threads: usize, worker_id: usize) -> Self {
-        let state = Arc::new(Mutex::new(MiningState {
+        let state = Arc::new(RwLock::new(MiningState {
             current_problem: None,
-            is_solved: false,
             solution_sender: None,
         }));
 
         let miner = Miner {
             worker_id,
             worker_name: format!("worker-{}", worker_id),
-            pow_computer: Arc::new(PowComputer::new()),
             num_threads,
             state,
         };
@@ -45,36 +43,59 @@ impl Miner {
         miner
     }
 
-    fn spawn_mining_threads(&self) {
-        let partition_size = U256::MAX / U256::from(self.num_threads);
+    fn compute_hash(vm: &RandomXVM, nonce: &U256, block_hash: &H256) -> H256 {
+        let mut input = [0u8; 64];
+        for i in 0..32 {
+            input[i] = block_hash[i];
+        }
+        nonce.to_little_endian(&mut input[32..64]);
 
+        let hash = vm.calculate_hash(&input).expect("Failed to calculate hash");
+        H256::from_slice(&hash)
+    }
+
+    fn calculate_nonce_range(
+        thread_id: usize, num_threads: usize, boundary: &U256,
+    ) -> (U256, U256) {
+        // Calculate total search space based on boundary
+        let total_space = U256::MAX / boundary;
+
+        // Calculate range size for each thread
+        let range_size = total_space / U256::from(num_threads);
+
+        // Calculate start and end for this thread
+        let start = U256::from(thread_id) * range_size * boundary;
+        let end = if thread_id == num_threads - 1 {
+            U256::MAX
+        } else {
+            (start + range_size * boundary) - U256::one()
+        };
+
+        (start, end)
+    }
+
+    fn spawn_mining_threads(&self) {
         for i in 0..self.num_threads {
             let state = Arc::clone(&self.state);
-            let pow = Arc::clone(&self.pow_computer);
-            let start_nonce = U256::from(i) * partition_size;
-            let nonce_range = partition_size;
             let worker_name = self.worker_name.clone();
+            let num_threads = self.num_threads;
 
-            info!(
-                "[{}] Spawning mining thread {} with nonce range start: {}",
-                worker_name, i, start_nonce
-            );
+            info!("[{}] Spawning mining thread {}", worker_name, i);
 
             thread::spawn(move || {
                 info!("[{}] Mining thread {} started", worker_name, i);
 
+                // Initialize RandomX VM for this thread
+                let flags = RandomXFlag::get_recommended_flags();
+                let vm = RandomXVM::new(flags, None, None)
+                    .expect("Failed to create RandomX VM");
+
+                let mut current_problem: Option<ProofOfWorkProblem> = None;
+
                 loop {
+                    // Read current problem state (no locks held during mining)
                     let (problem, sender) = {
-                        let state_guard = state.lock().unwrap();
-                        if state_guard.is_solved {
-                            trace!(
-                                "[{}] Thread {}: Problem solved, yielding",
-                                worker_name,
-                                i
-                            );
-                            thread::yield_now();
-                            continue;
-                        }
+                        let state_guard = state.read().unwrap();
                         (
                             state_guard.current_problem.clone(),
                             state_guard.solution_sender.clone(),
@@ -83,31 +104,57 @@ impl Miner {
 
                     match (problem, sender) {
                         (Some(problem), Some(sender)) => {
-                            trace!(
-                                "[{}] Thread {}: Mining with nonce start {}",
-                                worker_name,
-                                i,
-                                start_nonce
-                            );
-                            if let Some(solution) = pow.mine_range(
-                                &problem,
-                                start_nonce,
-                                Duration::from_secs(1),
-                            ) {
-                                let mut state_guard = state.lock().unwrap();
-                                if !state_guard.is_solved {
-                                    info!("[{}] Thread {}: Found solution with nonce {}", worker_name, i, solution.nonce);
-                                    state_guard.is_solved = true;
-                                    let _ = sender.send(solution);
+                            // Check if problem changed
+                            if current_problem.as_ref() != Some(&problem) {
+                                current_problem = Some(problem.clone());
+                                info!(
+                                    "[{}] Thread {}: New problem received, block_height: {}",
+                                    worker_name, i, problem.block_height
+                                );
+                            }
+
+                            let (start_nonce, end_nonce) =
+                                Self::calculate_nonce_range(
+                                    i,
+                                    num_threads,
+                                    &problem.boundary,
+                                );
+                            let mut current_nonce = start_nonce;
+                            while current_nonce < end_nonce {
+                                // Check for new problem periodically
+                                if current_nonce.low_u64() % CHECK_INTERVAL == 0
+                                {
+                                    let state_guard = state.read().unwrap();
+                                    if state_guard.current_problem.as_ref()
+                                        != Some(&problem)
+                                    {
+                                        break;
+                                    }
                                 }
+
+                                let hash = Self::compute_hash(
+                                    &vm,
+                                    &current_nonce,
+                                    &problem.block_hash,
+                                );
+                                let hash_u256 = U256::from(hash.as_bytes());
+
+                                if hash_u256 <= problem.boundary {
+                                    info!(
+                                        "[{}] Thread {}: Found solution with nonce {}",
+                                        worker_name, i, current_nonce
+                                    );
+                                    let solution = ProofOfWorkSolution {
+                                        nonce: current_nonce,
+                                    };
+                                    let _ = sender.send(solution);
+                                    break;
+                                }
+
+                                current_nonce += U256::one();
                             }
                         }
                         _ => {
-                            trace!(
-                                "[{}] Thread {}: No problem to solve, yielding",
-                                worker_name,
-                                i
-                            );
                             thread::yield_now();
                         }
                     }
@@ -117,14 +164,13 @@ impl Miner {
     }
 
     pub fn mine(
-        &self, problem: &ProofOfWorkProblem, timeout: Duration,
+        &self, problem: &ProofOfWorkProblem, timeout: std::time::Duration,
     ) -> Option<ProofOfWorkSolution> {
         let (tx, rx) = mpsc::channel();
 
         {
-            let mut state = self.state.lock().unwrap();
+            let mut state = self.state.write().unwrap();
             state.current_problem = Some(problem.clone());
-            state.is_solved = false;
             state.solution_sender = Some(tx);
         }
 

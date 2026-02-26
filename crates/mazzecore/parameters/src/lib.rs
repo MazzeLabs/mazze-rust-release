bc1cd54bab96ccf2aab3db43e910df8c8ad64ce7
Paladin0x <0xpaladin0x@gmail.com>
2026-02-26 08:53:41 +0200

    .

--- crates/mazzecore/parameters/src/lib.rs
+++ crates/mazzecore/parameters/src/lib.rs
@@ -74,8 +74,7 @@ pub mod consensus_internal {
 
     pub const GENESIS_TOKEN_COUNT_IN_MAZZE: u64 = 3_900_000_000;
     pub const MINING_SUPPLY_TARGET_IN_MAZZE: u64 = 2_500_000_000;
-    pub const MAX_SUPPLY_TOKEN_COUNT_IN_MAZZE: u64 =
-        GENESIS_TOKEN_COUNT_IN_MAZZE + MINING_SUPPLY_TARGET_IN_MAZZE;
+    pub const MAX_SUPPLY_TOKEN_COUNT_IN_MAZZE: u64 = GENESIS_TOKEN_COUNT_IN_MAZZE + MINING_SUPPLY_TARGET_IN_MAZZE;
     pub const HALVING_INTERVAL_IN_BLOCKS: u64 = 312_500_000;
 
     /// This is the cap of the size of the outlier barrier. If we have more
